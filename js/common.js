e08d015aa316d7ce4b8f57bf52b7b4a1d236f57f
CORP\A80876 <arielh3k@cnspartner.com>
2023-04-20 12:10:32 +0900

    ㅇㄹㄴㅇㄹ

--- js/common.js
+++ js/common.js
@@ -1,47 +1,61 @@
-var isMobile = /Android|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)
-  ? true
-  : false;
+// let bodyWidth = window.innerWidth;
+// let orDim = document.querySelector(".orientation-dim");
+// let bodysec = document.querySelector("body");
+// const formControl = document.querySelectorAll("input");
 
-if (isMobile) {
-  let bodyWidth = window.innerWidth;
-  let orDim = document.querySelector(".orientation-dim");
-  let bodysec = document.querySelector("body");
-  const formControl = document.querySelectorAll("input");
+// var ios = navigator.userAgent.match(/(iPod|iPhone)/);
+// if (ios) {
+//   window.addEventListener("orientationchange", (event) => {
+//     window.addEventListener("resize", () => {
+//       if (window.innerWidth > window.innerHeight) {
+//         landscapeDim();
+//       }
+//     });
+//   });
+// }
 
-  if (orDim) {
-    window.addEventListener("resize", () => {
-      let newWidth = window.innerWidth;
-      if (newWidth !== bodyWidth) {
-        bodyWidth = newWidth;
-        resizeDelay();
-        landscapeDim();
-      }
-      if (bodyWidth > 1000) {
-        orDim.classList.remove("is-show");
-      }
-    });
+// var isMobile = /Android|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)
+//   ? true
+//   : false;
 
-    function landscapeDim() {
-      if (window.matchMedia("(orientation: landscape)").matches) {
-        orDim.classList.add("is-show");
-        bodysec.style.overflow = "hidden";
-      } else if (window.matchMedia("(orientation: portrait)").matches) {
-        orDim.classList.remove("is-show");
-        bodysec.removeAttribute("style");
-      }
-    }
-    landscapeDim();
-  }
-}
+// if (isMobile) {
+//   if (orDim) {
+//     window.addEventListener("resize", () => {
+//       let newWidth = window.innerWidth;
+//       if (newWidth !== bodyWidth) {
+//         bodyWidth = newWidth;
+//         landscapeDim();
+//       }
+//       if (bodyWidth > 1000) {
+//         orDim.classList.remove("is-show");
+//       }
+//     });
 
-function resizeDelay() {
-  let delay = 100;
-  let timer = null;
-  clearTimeout(timer);
-  timer = setTimeout(() => {
-    document.location.reload();
-  }, delay);
-}
+//     function landscapeDim() {
+//       if (window.matchMedia("(orientation: landscape)").matches) {
+//         orDim.classList.add("is-show");
+//         bodysec.style.overflow = "hidden";
+//         formControl.forEach((inp) => {
+//           inp.blur();
+//           console.log(inp);
+//         });
+//       } else if (window.matchMedia("(orientation: portrait)").matches) {
+//         orDim.classList.remove("is-show");
+//         bodysec.removeAttribute("style");
+//       }
+//     }
+//     landscapeDim();
+//   }
+// }
+
+// function resizeDelay() {
+//   let delay = 100;
+//   let timer = null;
+//   clearTimeout(timer);
+//   timer = setTimeout(() => {
+//     document.location.reload();
+//   }, delay);
+// }
 
 // GNG
 const headerLayout = document.querySelector(".header-layout");
@@ -93,109 +107,3 @@ function fullMenuClose() {
     fullMenu.removeChild(dim);
   }
 }
-
-// tab
-const tabWrap = document.querySelector(".tab-wrap");
-const tabs = document.querySelector(".tabs");
-const tabBtns = document.querySelectorAll(".tabs .tab-btn");
-const tabPanel = document.querySelectorAll(".tab-wrap .tab-content");
-if (tabWrap) {
-  tabs.addEventListener("click", (e) => {
-    const id = e.target.dataset.id;
-    if (id) {
-      tabBtns.forEach((btn) => {
-        btn.classList.remove("active");
-      });
-      e.target.classList.add("active");
-      tabPanel.forEach((panel) => {
-        panel.classList.remove("active");
-      });
-      const el = document.getElementById(id);
-      el.classList.add("active");
-    }
-  });
-}
-
-// accordian
-const accordianWrap = document.querySelector(".type-accordian");
-const accordians = document.querySelectorAll(
-  ".bbs-list.type-accordian .list-item"
-);
-if (accordianWrap) {
-  accordians.forEach((accordian) => {
-    const accBtn = accordian.querySelector(".acc-toggle");
-    accBtn.addEventListener("click", () => {
-      // accordians.forEach((item) => {
-      //     if(item !== accordian) {
-      //         item.classList.remove('active')
-      //     }
-      // })
-      accordian.classList.toggle("active");
-    });
-  });
-}
-
-const selectContainer = document.querySelector(".custom-select");
-const selectBox = document.querySelector(".custom-select");
-const selectBtn = document.querySelector(".select-btn");
-const options = document.querySelectorAll(".option-item");
-if (selectContainer) {
-  const handleSelect = (item) => {
-    selectBtn.innerHTML = item.textContent;
-    selectBox.classList.remove("active");
-  };
-  options.forEach((option) => {
-    option.addEventListener("click", () => {
-      handleSelect(option);
-    });
-  });
-
-  selectBtn.addEventListener("click", () => {
-    if (selectBox.classList.contains("active")) {
-      selectBox.classList.remove("active");
-    } else {
-      selectBox.classList.add("active");
-    }
-  });
-}
-
-// modal
-const modalSection = document.querySelector("body");
-const modals = document.querySelectorAll(".modal");
-const modalClose = document.querySelectorAll(".modal-close");
-if (modalSection) {
-  modalSection.addEventListener("click", (e) => {
-    const modalId = e.target.dataset.modal;
-    if (modalId) {
-      const modalLayer = document.getElementById(modalId);
-      modalLayer.classList.add("is-show");
-      modalSection.style.overflow = "hidden";
-    }
-  });
-
-  modalClose.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      modals.forEach((modal) => {
-        modal.classList.remove("is-show");
-        modalSection.removeAttribute("style");
-      });
-    });
-  });
-}
-
-// 로그인 팝업 clear 2023-03-29
-const loginField = document.querySelectorAll(".form-item");
-loginField.forEach((field) => {
-  const inputItem = field.querySelector(".inp-unset");
-  const value = inputItem.value;
-  const clear = field.querySelector(".btn-input-clear");
-
-  inputItem.addEventListener("keydown", () => {
-    clear.style.display = "block";
-    clear.classList.remove("is-hide");
-  });
-  clear.addEventListener("click", () => {
-    inputItem.value = "";
-    clear.classList.add("is-hide");
-  });
-});
