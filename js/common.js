80f228e5f527df547dac54ba4b0161ab3599d145
CORP\A80876 <arielh3k@cnspartner.com>
2023-04-25 18:28:25 +0900

    dfsdfsdf

--- js/common.js
+++ js/common.js
@@ -5,15 +5,17 @@ const formControl = document.querySelectorAll("input");
 
 var ios = navigator.userAgent.match(/(iPod|iPhone)/);
 if (ios) {
-  window.addEventListener("orientationchange", () => {
-    if (window.matchMedia("(orientation: landscape)").matches) {
-      formControl.forEach((inp) => {
-        inp.blur();
-      });
-    } else if (window.matchMedia("(orientation: portrait)").matches) {
-      window.scrollTo(0, 0);
-      window.document.body.scrollTop = 0;
-    }
+  screen.addEventListener("orientationchange", () => {
+    screen.addEventListener("resize", () => {
+      if (window.matchMedia("(orientation: landscape)").matches) {
+        formControl.forEach((inp) => {
+          inp.blur();
+        });
+      } else if (screen.matchMedia("(orientation: portrait)").matches) {
+        screen.scrollTo(0, 0);
+        screen.document.body.scrollTop = 0;
+      }
+    });
   });
 }
 
@@ -22,8 +24,8 @@ var aos = /Android|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)
   : false;
 
 if (aos) {
-  window.addEventListener("resize", () => {
-    if (window.matchMedia("(orientation: landscape)").matches) {
+  screen.addEventListener("resize", () => {
+    if (screen.matchMedia("(orientation: landscape)").matches) {
       formControl.forEach((inp) => {
         inp.blur();
       });
